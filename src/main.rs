ba083f13d10b186671daf710d6873ca887afea67
MyselfLeo <myselfleo@outlook.fr>
2022-01-25 15:34:37 +0100

    TODO: use geocodeArea instead of Nominatim

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod overpass;
 
 
 fn main() {
+    /*
     let mut searcher = nominatim::Searcher::new();
     let mut map_data = overpass::MapData::new();
 
@@ -12,4 +13,12 @@ fn main() {
     let result = map_data.request(bounding_box);
 
     println!("{}", result);
+    */
+    let mut searcher = nominatim::Searcher::new();
+    let bounding_box = searcher.research("Chazey-sur-Ain, France");
+
+    let mut request_builder = overpass::RequestBuilder::new(bounding_box);
+    request_builder.set_details_level(2);
+    println!("{}", request_builder.get_request_txt(Some(true)));
+
 }
--- src/overpass.rs
+++ src/overpass.rs
@@ -5,6 +5,82 @@ use reqwest;
 static API_URL: &str = "https://lz4.overpass-api.de/api/interpreter";
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
+static HIGHWAY_LEVELS: [&str; 7] = ["residential", "unclassified", "tertiary", "secondary", "primary", "trunk", "motorway"];
+static WATERWAY_LEVELS: [&str; 6] = ["ditch", "drain", "stream", "river", "canal", "riverbank"];
+
+
+
+
+/// Struct used to generate the text to pass to Overpass API
+pub struct RequestBuilder {
+    details_level: u8, // Betwenn 0 and 6. 0 is the most detailed.
+    get_building: bool, // If true, the request will ONLY return buildings (the center of their geometry
+    bounding_box: [String; 4],
+    timeout: u32,
+}
+
+impl RequestBuilder {
+
+    /// Create a new RequestBuilder. The bounding_box parameter is the bounding box of the research
+    pub fn new(bounding_box: [String; 4]) -> RequestBuilder {
+        RequestBuilder {
+            details_level: 0,
+            get_building: false,
+            bounding_box: bounding_box,
+            timeout: 30,
+        }
+    }
+
+    /// Set whether to get the buildings or not.
+    pub fn get_building(&mut self, value: bool) {
+        self.get_building = value;
+    }
+
+    /// Set the details level of the request.
+    pub fn set_details_level(&mut self, value: u8) {
+        if value > 6 {
+            panic!("The details level must be between 0 and 6");
+        }
+        self.details_level = value;
+    }
+
+    /// Build the request text to pass to Overpass API
+    pub fn get_request_txt(&self, with_newline: Option<bool>) -> String {
+        let mut request: String = format!("[out:json][timeout:{}];\n", self.timeout); // Base parameters for the request
+
+        if self.get_building {
+            request.push_str(format!("way[building]({}); out center;\n", self.bounding_box.join(",")).as_str());
+        }
+
+        else {
+            // List every level of detail required
+            request.push_str("((");
+
+            for x in self.details_level..6 {
+                request.push_str(format!("way[highway={}]({});\n", HIGHWAY_LEVELS[x as usize], self.bounding_box.join(",")).as_str());
+                request.push_str(format!("way[waterway={}]({});\n", WATERWAY_LEVELS[x as usize], self.bounding_box.join(",")).as_str());
+            }
+            request.push_str(format!("way[highway={}]({});\n", HIGHWAY_LEVELS[6], self.bounding_box.join(",")).as_str());
+            request.push_str(");\nnode(w);\n);\nout;\n");
+        }
+
+        let keep_newline: bool = match with_newline {
+            Some(value) => value,
+            None => false,
+        };
+
+        if keep_newline {request}
+        else {request.replace('\n', "")}
+    }
+}
+
+
+
+
+
+
+
+
 
 
 /// Struct used to store overpass cache and to make requests to overpass
@@ -41,7 +117,7 @@ impl MapData {
         // If not cached, make a request to the Overpass API and cache the result
         else {
             // Generate request data
-            let data = format!("((way({})[highway];way({})[waterway];);node(w););out;", key, key);
+            let data = format!("((way({})[highway=primary];way({})[waterway=river];);node(w););out;", key, key);
 
             // Create the URL of the request
             let request_url = format!("{}?data={}", API_URL, data);
